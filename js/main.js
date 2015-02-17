d891d9ce9c9dbb0a8c5aabe3f5e0fdbbd44fd3c8
Jon Rutishauser <jrutishauser@gmail.com>
2015-02-17 15:25:56 -0500

    button shows random text when pressed

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+	var inspirebutton = document.getElementById('inspire');	
+	var quotehere = document.getElementById('quotehere'); 	
+	
+
+
+
+	var quoteDrop = function() {
+		quotehere.innerHTML = 'Youll Never finish this in time';	
+		console.log('wat');	
+	};
+	inspirebutton.onclick = quoteDrop; 
+
+
+
+
+
+
+
+
+
+
+
+
 
