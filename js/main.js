0c35bb05cb6f653d7e8700356a550704cfbe02b0
Evan Klemen <ejklemen@gmail.com>
2015-02-17 17:33:04 -0500

    concatinated the text part of the url, then imported my questions.js array to the main.js using a math.random to choose a random question from the array and insert that into the post function

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-	var inspirebutton = document.getElementById('inspire');	
+
+    var inspirebutton = document.getElementById('inspire');	
 	var quotehere = document.getElementById('quotehere'); 	
 	
 
@@ -10,16 +11,26 @@
 	};
 	inspirebutton.onclick = quoteDrop; 
 
+    
+$('.question').on('click', function (e){
+    
+    var questions = [
+    'If you were a vegetable what would you be and why?',
+    'If you could travel to any country, where would you go?',
+    'If you could have a beer(or other drink) with any president, who would you choose?',
+    'Describe your personality as a breakfast cereal (capn crunch, lucky charms etc)',
+    'If you had to eat just one meal for the rest of your life what would it be?',
+    'As a child, what did you want to be when you grew up?',
+    'Which season(fall, winter, spring, summer) would you want to be all year long?',
+    'What movie/TV/book character would you choose to be for a day?',
+    'If you could choose a super power, what would it be and why?',
+    'Are you a cat or a dog person, and why did you choose cat?'
+];
 
-
-//curl -X POST --data-urlencode 'payload={"text": "This is posted to <#susannabottest> and comes from *Mybot*.", "channel": "#susannabottest", "username": "Mybot", "icon_emoji": ":monkey_face:"}' https://slack.com/api/chat.postMessage\?token\=xoxp-3520991751-3641328250-3752113976-71ae8c\&channel\=%23susannabottest\&text\=Hi%20there\!\&username\=SusannaBot\&icon_emoji\=%3Amonkey_face%3A\&pretty\=1
-
-//$.post('https://slack.com/api/chat.postMessage\?token\=xoxp-3520991751-3641328250-3752113976-71ae8c\&channel\=%23susannabottest\&text\=Hi%20there\!\&username\=SusannaBot\&icon_emoji\=%3Amonkey_face%3A\&pretty\=1');
-//$.post()
-
-
-
-
-
-
+var randomQuestion = function(){
+    return questions[Math.floor(Math.random()*questions.length)];
+}  
+    
+    $.post('https://slack.com/api/chat.postMessage\?token\=xoxp-3520991751-3641328250-3752113976-71ae8c\&channel\=%23susannabottest\&text\=' + randomQuestion() + '&username\=SusannaBot\&icon_emoji\=%3Amonkey_face%3A\&pretty\=1');
+})
 
