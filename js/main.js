2adff515b0eccf5e847808e69d8d02787059fdd0
Evan Klemen <ejklemen@gmail.com>
2015-02-17 16:16:57 -0500

    downloaded jquery instead of using cdn

--- js/main.js
+++ js/main.js
@@ -12,9 +12,10 @@
 
 
 
+//curl -X POST --data-urlencode 'payload={"text": "This is posted to <#susannabottest> and comes from *Mybot*.", "channel": "#susannabottest", "username": "Mybot", "icon_emoji": ":monkey_face:"}' https://slack.com/api/chat.postMessage\?token\=xoxp-3520991751-3641328250-3752113976-71ae8c\&channel\=%23susannabottest\&text\=Hi%20there\!\&username\=SusannaBot\&icon_emoji\=%3Amonkey_face%3A\&pretty\=1
 
-
-
+//$.post('https://slack.com/api/chat.postMessage\?token\=xoxp-3520991751-3641328250-3752113976-71ae8c\&channel\=%23susannabottest\&text\=Hi%20there\!\&username\=SusannaBot\&icon_emoji\=%3Amonkey_face%3A\&pretty\=1');
+//$.post()
 
 
 
