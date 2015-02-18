df5da7566f9d02c46e28217619d00ab2620defe2
Evan Klemen <ejklemen@gmail.com>
2015-02-18 10:07:53 -0500

    cleaned up main.js and popup.html removed white space and beautified, also removed h5bp excess files

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
+var inspirebutton = document.getElementById('inspire');
+var quotehere = document.getElementById('quotehere');
 
-    var inspirebutton = document.getElementById('inspire');	
-	var quotehere = document.getElementById('quotehere'); 	
-	
-	var adviceApi = 'http://api.adviceslip.com/advice';
-	$.getJSON( adviceApi, function(advice) {
-			var quoteDrop = function() {
-			$('#quotehere').text(advice.slip.advice);	
-			console.log('wat');	
-		};
-		inspirebutton.onclick = quoteDrop; 	
-	});
-	
-	$('.schedule').on('click', function(){
-		window.open("https://calendly.com/susanna", '_blank');			
-	});
-    
-$('.question').on('click', function (e){
-    
+var adviceApi = 'http://api.adviceslip.com/advice';
+$.getJSON(adviceApi, function (advice) {
+    var quoteDrop = function () {
+        $('#quotehere').text(advice.slip.advice);
+        console.log('wat');
+    };
+    inspirebutton.onclick = quoteDrop;
+});
+
+$('.schedule').on('click', function () {
+    window.open("https://calendly.com/susanna", '_blank');
+});
+
+$('.question').on('click', function (e) {
+
     var questions = [
     'If you were a vegetable what would you be and why?',
     'If you could travel to any country, where would you go?',
@@ -31,24 +30,20 @@ $('.question').on('click', function (e){
     'You win a free (up-to) 3-topping pizza, what do you choose?'
 ];
 
-var randomQuestion = function(){
-    return questions[Math.floor(Math.random()*questions.length)];
-};  
+    var randomQuestion = function () {
+        return questions[Math.floor(Math.random() * questions.length)];
+    };
     $('#quotehere').text(randomQuestion);
-    var currentQ = $('#quotehere').text(); 
-	$('#quotehere').append('<fieldset><input id="answer" type="text" placeholder="Answer here">' + 
-                           '<input id="submit" type="submit" value="Slack It!"></input></input></fieldset>');
-    $('#submit').on('click', function(event){
+    var currentQ = $('#quotehere').text();
+    $('#quotehere').append('<fieldset><input id="answer" type="text" placeholder="Answer here">' +
+        '<input id="submit" type="submit" value="Slack It!"></input></input></fieldset>');
+    $('#submit').on('click', function (event) {
         var ans = $('#answer').val();
         $.post('https://slack.com/api/chat.postMessage\?token\=' +
-		'xoxp-3520991751-3641328250-3752113976-71ae8c\&channel\=%2' +
-		'3susannabottest\&text\=' + '>' + currentQ + '  ' + '*' + ans + '*' + '&username\=' + 
-		'SusannaBot\&icon_emoji\=%3Amonkey_face%3A\&pretty\=1');
-	$('#quotehere').empty();	
-	});
-    
-});
-    
+            'xoxp-3520991751-3641328250-3752113976-71ae8c\&channel\=%2' +
+            '3susannabottest\&text\=' + '>' + currentQ + '  ' + '*' +
+            ans + '*' + '&username\=' + 'SusannaBot\&icon_emoji\=%3Amonkey_face%3A\&pretty\=1');
+        $('#quotehere').empty();
+    }); // onclick for submit answer to slack button
 
-
-
+}); //onclick for generating a random question
